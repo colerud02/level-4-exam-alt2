1ff9f5f62c00e6d17c009f942bcd24443fcccfaa
100297 <league@WTS7.attlocal.net>
2017-08-31 18:24:20 -0700

    a
    a

--- src/CountingDuplicates.java
+++ src/CountingDuplicates.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class CountingDuplicates {
 	public static int duplicateCount(String word) {
+		word.toLowerCase();
+		char[] array = word.toCharArray();
+		char letter;
+		int num = 0;
+		for (int i = 0; i < array.length; i++) {
+			letter = array[i];
+			for (int j = 0; j < array.length; j++) {
+				if (letter == array[j]) {
+					num++;
+					//break;
+				}
+			}
+			return num;
+		}
 		return 0;
 	}
 	
